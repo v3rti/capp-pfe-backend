9e98dd062741da91edd011e8cdb6dcd99fd029b3
Amine <med.bagui.1@gmail.com>
2021-05-08 15:28:19 +0000

    ability to join convos from postman working now, each new user will get pushed into an array of already joined users, same for messages, need to handle from the front end

--- routes/activeConvos.js
+++ routes/activeConvos.js
@@ -9,12 +9,61 @@ router.get('/',(req,res) => {
 })
 
 
-router.post('/',(req,res) => {
-  const {cuid,user_joined,chats} = req.body;
+router.post('/', async (req,res) => {
+  const {cuid,users_joined,chats} = req.body;
   
-  const xd = user_joined.map(user => user.fullName)
-  res.send(xd);
+  const aConvo = new ActiveConversations({
+    cuid,
+    users_joined,
+    chats
+  })
+  try {
+  const savedAConvo = await aConvo.save();
+  res.json(savedAConvo);
+  }catch(err){
+    res.json({message: err});
+  }
 
+})
+
+router.put('/:id', async (req,res) => {
+  const {message,sender,date} = req.body;
+  const xd = {
+    message: message,
+    messageDate: date,
+    senderUsername: sender
+  }
+  try{
+    const result = await ActiveConversations.updateOne(
+      { cuid: req.params.id}, 
+      { $push: { chats: xd } }
+  );
+  res.json(result);
+  }catch(err){
+    res.json({message: err})
+    console.log(err);
+  }
+ 
+})
+
+router.put('/userjoin/:id', async (req,res) => {
+  const {fullName,username,date} = req.body;
+  const xd = {
+    fullName,
+    username,
+    joinedDate: date
+  }
+  try{
+    const result = await ActiveConversations.updateOne(
+      { cuid: req.params.id}, 
+      { $push: { users_joined: xd } }
+  );
+  res.json(result);
+  }catch(err){
+    res.json({message: err})
+    console.log(err);
+  }
+ 
 })
 
 
